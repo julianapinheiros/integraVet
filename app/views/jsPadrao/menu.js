5a257d673f76042e29bff693202932ed50ace39c
Pinheiro-98 <julianasilva180198@gmail.com>
2023-11-16 23:10:16 -0300

    Commit Ju

--- app/views/jsPadrao/menu.js
+++ app/views/jsPadrao/menu.js
@@ -1,9 +1,19 @@
-const headerLinks = document.querySelectorAll("div .navbar-collapse ul a");
+// const headerLinks = document.querySelectorAll("div .navbar-collapse ul a");
 
-headerLinks.forEach((link, index) => {
-  if(link.href === document.location.href) {
-    headerLinks[index].classList.add("current")
+// headerLinks.forEach((link, index) => {
+//   if(link.href === document.location.href) {
+//     headerLinks[index].classList.add("current")
+//   } else {
+//     return;
+//   }
+// });
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
   } else {
-    return;
+    x.className = "topnav";
   }
-});
+}
